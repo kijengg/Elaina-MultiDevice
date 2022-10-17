6865930fed8f4a568eecb62dca2209a495a7c31c
kijengg <115552596+kijengg@users.noreply.github.com>
2022-10-17 18:52:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -59,9 +59,9 @@ global.ppulsa = '085736178354' //Nomor SimCard Yang Kamu Pake
 global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6281231079387' //Nomor Bot
+global.nomorbot = '6288991344802' //Nomor Bot
 global.nomorown = '6285736178354' //Nomor Owner
-global.namebot = 'Elaina BOT' //Nama Bot
+global.namebot = 'KyzheBOT-MD' //Nama Bot
 global.nameown = '𝓧𝓲𝓪𝓸 - 𝓨𝓪𝓷' // Nama Owner
 
 
